391456e706b2000a79f76ebca360afc424a0717d
David THIBAU <david.thibau@gmail.com>
2021-01-19 17:03:00 +0100

    Configuration Postgres, profil de prod

--- tp2/src/test/java/org/formation/repository/DocumentRepositoryTest.java
+++ tp2/src/test/java/org/formation/repository/DocumentRepositoryTest.java
@@ -1,9 +1,5 @@
 package org.formation.repository;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Date;
 import java.util.List;
 
 import org.formation.model.Document;
@@ -29,8 +25,7 @@ class DocumentRepositoryTest {
 		
 		 List<Document> documents = documentRepository.findByOwner(id1);
 		
-		 documents.stream().forEach(System.out::println);
-		 	
+		 documents.stream().forEach(System.out::println);	 	
 	}
 	
 	@Test
@@ -41,41 +36,5 @@ class DocumentRepositoryTest {
 		
 		 documents.stream().forEach(System.out::println);
 		 	
-	}
-	
-	@Test
-	void testingCascading() {
-		int initialMemberCount = memberRepository.findAll().size();
-		int initialDocumentCount = documentRepository.findAll().size();
-		
-		Member newMember = new Member();
-		newMember.setAge(18);
-		newMember.setEmail("tutu@mail.com");
-		newMember.setPassword("secret");
-
-		Document doc = new Document();
-		doc.setContentType("dummy");
-		doc.setName("Dummy.doc");
-		doc.setUploadedDate(new Date());
-		Document doc2 = new Document();
-		doc.setContentType("dummy");
-		doc.setName("Dummy2.doc");
-		doc.setUploadedDate(new Date());
-
-		newMember.addDocument(doc);
-		newMember.addDocument(doc2);
-		
-		memberRepository.save(newMember);
-		
-		assertAll("Adding One Member 2 docs",
-                () -> assertEquals(initialMemberCount+1,memberRepository.findAll().size()),
-                () -> assertEquals(initialDocumentCount+2, documentRepository.findAll().size())
-        );
-			
-	}
-
-	@Test
-	void testingFullLoad() {
-		
 	}
 }
