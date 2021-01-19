1e8db09ce5c3a2ff65a00567f2fee3591fd6fbc3
David THIBAU <david.thibau@gmail.com>
2021-01-19 14:06:37 +0100

    Profils et configuration des traces

--- tp1/src/main/java/org/formation/HelloController.java
+++ tp1/src/main/java/org/formation/HelloController.java
@@ -2,21 +2,36 @@ package org.formation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Profile({"prod","integration"})
 public class HelloController {
 	@Autowired
 	HelloProperties props;
-	
+
 	@Value("${app.random}")
 	String uuid;
 
 	@RequestMapping("/hello")
 	public String hello(@RequestParam String name) {
-		return props.getGreeting() + name;
+		switch (props.getCaseType()) {
+		case LOWER:
+			name = name.toLowerCase();
+			break;
+		case UPPER:
+			name = name.toUpperCase();
+			break;
+		default:
+			break;
+		}
+		if (props.getPosition() == 0)
+			return props.getGreeting() + name;
+		else
+			return name + props.getGreeting();
 	}
 
 	@RequestMapping("/uuid")
