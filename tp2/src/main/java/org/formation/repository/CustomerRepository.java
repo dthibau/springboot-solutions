71cac3dfe12b1a97489be6d698d3a58f0d435413
David THIBAU <david.thibau@gmail.com>
2021-01-20 10:37:10 +0100

    Exemples Mongo

---
+++ tp2/src/main/java/org/formation/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package org.formation.repository;
+
+import java.util.List;
+
+import org.formation.model.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+	
+	public List<Customer> findByLastName(String lastName);
+	
+	public List<Customer> findByFirstName(String firstName);
+	
+	public List<Customer> findByFirstNameAndLastName(String firstName, String lastName);
+	
+
+}
---
+++ tp2/src/test/java/org/formation/service/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package org.formation.service;
+
+import java.util.Date;
+
+import org.formation.model.Document;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemberServiceTest {
+
+	@Autowired
+	MemberService memberService;
+	
+	@Test
+	void testTransaction() {
+		Document doc = new Document();
+		doc.setContentType("dummy");
+		doc.setName("Dummy.doc");
+		doc.setUploadedDate(new Date());
+		
+		memberService.addDocToAllMembers(doc);
+	}
+	
+	@Test
+	void testTransactionWithException() throws Exception {
+		Document doc = new Document();
+		doc.setContentType("dummy");
+		doc.setName("Dummy.doc");
+		doc.setUploadedDate(new Date());
+		
+		memberService.addDocToAllMembersWithException(doc);
+	}
+}
