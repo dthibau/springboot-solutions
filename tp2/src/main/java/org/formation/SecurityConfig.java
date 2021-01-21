4a7e8741ca4770b1399b179ff08a21f2561cbd75
David THIBAU <david.thibau@gmail.com>
2021-01-21 14:04:41 +0100

    Utilisateurs mémoire, USER et ADMIN

--- tp2/src/main/java/org/formation/SecurityConfig.java
+++ tp2/src/main/java/org/formation/SecurityConfig.java
@@ -2,6 +2,8 @@ package org.formation;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,15 +17,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.csrf().disable().authorizeRequests()
 		   .antMatchers("/swagger-ui.html").permitAll()
 		   .antMatchers(HttpMethod.GET, "/api/**").authenticated().antMatchers("/api/**").hasRole("ADMIN")
 		   .anyRequest().authenticated()
 		   .and()
 		   .formLogin()
-		   .and().
-		   logout().logoutSuccessUrl("/home").logoutUrl("/logout").invalidateHttpSession(true).permitAll();
+		   .and()
+		   .logout().logoutSuccessUrl("/home").logoutUrl("/logout").invalidateHttpSession(true).permitAll();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("user").password("{noop}secret").roles("USER")
+		.and()
+		.withUser("admin").password("{noop}secret").roles("ADMIN");	
 	}
 
 }
