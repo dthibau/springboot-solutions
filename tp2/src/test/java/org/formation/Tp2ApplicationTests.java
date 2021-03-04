2c270cca62119d6eb379e3dea3afe59c0236106f
David THIBAU <david.thibau@gmail.com>
2021-03-04 08:42:02 +0100

    Tests complets avec RestTemplate

--- tp2/src/test/java/org/formation/Tp2ApplicationTests.java
+++ tp2/src/test/java/org/formation/Tp2ApplicationTests.java
@@ -1,13 +1,43 @@
 package org.formation;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.formation.model.Member;
+import org.hibernate.LazyInitializationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT, 
+properties = {"security.user.name= user", "security.user.password= secret"})
 class Tp2ApplicationTests {
 
+	@Autowired
+	RestTemplateBuilder restTemplateBuilder;
+	
+	private RestTemplate restTemplate;
+	
+	@BeforeEach
+	public void setup() {
+		this.restTemplate = restTemplateBuilder.basicAuthentication("dthibau@wmmod.com", "secret").rootUri("http://localhost:8080").build();
+	}
+	
 	@Test
-	void contextLoads() {
+	void findById() {
+		
+		Member m = restTemplate.getForObject("/api/members/1", Member.class);
+		
+		assertAll(() -> assertEquals(1, m.getId()),
+				  () -> assertTrue(m.getDocuments().isEmpty()));
+		
 	}
 
 }
